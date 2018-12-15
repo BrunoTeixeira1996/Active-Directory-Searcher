f60fa2be766692342a9a36c9f26c20e2272156e2
BrunoTeixeira1996 <brunoalexandre3@hotmail.com>
2018-12-15 21:52:44 +0000

    first one

---
+++ README.md
@@ -0,0 +1,28 @@
+# Ad Searcher
+A script to search for users in Microsoft Active Directory.
+It lists usefull information about a chosen user in a domain.
+- Name
+- Email
+- Account Created
+- Password Last Set
+- Logon Count
+- Last Logon
+- Last Logoff
+- Account Expires
+- Member Off
+
+# Requirements
+  - Python3
+  - LDAP3 [https://github.com/cannatag/ldap3]
+  - Colorama [https://pypi.org/project/colorama/]
+  - Parser [https://dateutil.readthedocs.io/en/stable/parser.html]
+  - Windows Server Machine
+
+# ScreenShot
+![image](https://user-images.githubusercontent.com/12052283/50047677-4dbe3300-00b2-11e9-9442-58b2d1738a37.png) 
+
+# Contribute
+Want to contribute? Go ahead!
+
+# License
+![image](https://user-images.githubusercontent.com/12052283/50047602-7a714b00-00b0-11e9-8473-f6256e84c75e.png)
---
+++ ad.py
@@ -0,0 +1,47 @@
+from ldap3 import *
+from ldap3.core.exceptions import *
+from colorama import *
+from dateutil.parser import parse
+
+
+server_name = 'YOURSERVERNAME WITH .[YOUR TLD]'
+domain_name = 'YOURSERVERNAME'
+user_name = 'YOURSERVERNAME WITH .[YOUR TLD]\Administrator'
+password = 'YOURPASSWORD'
+
+print(Fore.GREEN+''' 
+==================================
+|                                |
+|                                |
+|   Active Directory Finder      |
+|                                |
+|       by: Bruno Teixeira       |
+|                                |
+|                                |
+==================================
+	''')
+
+def connection_to_ad():
+	server = Server(server_name, get_info = ALL)
+	conn = Connection(server, user = user_name, password = password,  authentication = NTLM, auto_bind = True)
+	conn.search('dc = {}, dc = pt'.format(domain_name), '(objectclass=person)', attributes = [ALL_ATTRIBUTES,ALL_OPERATIONAL_ATTRIBUTES])
+	return conn
+
+
+def search(conn):
+	name = input('Name: ')
+	for people in conn.entries:
+		if name == people['sAMAccountName']:
+			created = (parse(people['whenCreated'].value.strftime('%Y-%m-%d %H:%M:%S')))
+			email = people['userPrincipalName']
+			logoncount = people['logonCount']
+			pwdLastSet = (parse(people['pwdLastSet'].value.strftime('%Y-%m-%d %H:%M:%S')))
+			lastlogon = (parse(people['lastLogon'].value.strftime('%Y-%m-%d %H:%M:%S')))
+			lastlogoff = (parse(people['lastLogoff'].value.strftime('%Y-%m-%d %H:%M:%S')))
+			accexpires = (parse(people['accountExpires'].value.strftime('%Y-%m-%d %H:%M:%S')))
+			memberoff = people['memberOf']
+			print(f'\nName:{name}\n\nEmail:{email}\n\nAccountCreated:{created}\n\nPasswordLastSet:{pwdLastSet}\n\nLogonCount:{logoncount}\n\nLastLogon:{lastlogon}\n\nLastLogoff:{lastlogoff}\n\nAccountExpires:{accexpires}\n\nMemberOff:{memberoff}')
+			print(Style.RESET_ALL)
+if __name__ == '__main__':
+	conn = connection_to_ad()
+search(conn)
